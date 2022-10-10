54926b8f35d1fe667ff3cc5f0e22f1e303bcb55a
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-10-10 12:23:54 -0300

    Alteração

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,7 @@
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiroPaciente")
+
+var tdPeso = document.querySelector(".info")
+document.write(paciente.textContent)
