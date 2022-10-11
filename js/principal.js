8b8f5ddc69021577e3f3f4c35f7901ad0c98df1a
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-10-10 22:30:40 -0300

    Alteração

--- js/principal.js
+++ js/principal.js
@@ -5,11 +5,29 @@ var paciente = document.querySelector("#primeiroPaciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura")
+var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
-
-var imc = peso / (altura * altura);
 
 var tdImc = document.querySelector(".info-imc");
 
-tdImc.textContent = imc;
+var pesoEhValido = true;
+var alturaEhValida = true;
+
+if (peso <= 0 || peso >= 500) {
+    document.write("Peso Inválido!")
+    pesoEhValido = false;
+    tdImc.textContent = ("Peso Inválido!")
+}
+
+// Barra dupla significa "ou" se um item é uma "ou" outra opção.
+
+if (altura <= 0 || altura >= 3) {
+    document.write("Altura Inválida!")
+    alturaEhValida = false;
+    tdImc.textContent = ("Altura Inválido!")    
+}
+
+if (pesoEhValido && alturaEhValida) {
+    var imc = peso / (altura * altura);
+    tdImc.textContent = imc;
+}
