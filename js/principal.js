9897befc113d6bf5b7a89d4a490c7a9a348e2ad4
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-10-13 22:57:21 -0300

    Alteração

--- js/principal.js
+++ js/principal.js
@@ -39,12 +39,45 @@ for (var i = 0; i < pacientes.length; i++) {
         tdImc.textContent = (imc.toFixed(2))
     }
 }
-titulo.addEventListener("click",);
 
 function mostraMensagem(){
     document.write("Ola eu fui clicado!");
 }
-var botaoAdicionar = document.querySelector("#adiciconar-paciente");
-botaoAdicionar.addEventListener("click", function() {
-    document.write("Fui clicado!");
-})
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    //event.preventDefault() previne as ações padrão do brownser.
+
+    var form = document.querySelector("#form-adiciona");
+     var nome = form.nome.value;
+     var peso = form.peso.value;
+     var altura = form.altura.value;
+     var gordura = form.gordura.value;
+    
+    var pacienteTr = document.createElement("tr")
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = imc.toFixed(2);
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+});
+
+titulo.addEventListener("click",function (){
+    document.write("fui clicado!")
+});
