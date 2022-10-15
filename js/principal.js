11d7d63d4e0dfe63585023588f9d35d60145fd7b
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-10-15 19:56:50 -0300

    Alteração

--- js/principal.js
+++ js/principal.js
@@ -35,54 +35,14 @@ for (var i = 0; i < pacientes.length; i++) {
     }
 
     if (pesoEhValido && alturaEhValida) {
-        var imc = peso / (altura * altura)
-        tdImc.textContent = (imc.toFixed(2))
+        var imc = calculaImc(peso,altura)
+        tdImc.textContent = imc;
     }
 }
 
-/*function mostraMensagem(){
-    document.write("Ola eu fui clicado!");
-    Exemplo de função nomeada.
-}*/
+function calculaImc(peso,altura){
+    var imc = 0;
+    imc = peso / (altura*altura);
 
-
-var botaoAdicionar = document.querySelector("#adicionar-paciente");
-botaoAdicionar.addEventListener("click", function(event) {
-    event.preventDefault();
-    //event.preventDefault() previne as ações padrão do brownser.
-
-    var form = document.querySelector("#form-adiciona");
-     var nome = form.nome.value;
-     var peso = form.peso.value;
-     var altura = form.altura.value;
-     var gordura = form.gordura.value;
-    
-    var pacienteTr = document.createElement("tr")
-
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
-
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = imc.toFixed(2);
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
-});
-
-
-/*titulo.addEventListener("click",function (){
-    document.write("fui clicado!")
-    Exemplo de função anonima.
-});*/
+    return imc.toFixed(2);
+}
