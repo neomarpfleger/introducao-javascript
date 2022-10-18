3a9d6e5a98d0c29809774fcc99c64cb5d1e71228
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-10-17 22:29:16 -0300

    17102022
    Alteração do código para boas praticas.

--- js/form.js
+++ js/form.js
@@ -1,33 +1,14 @@
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event) {
+
     event.preventDefault();
     //event.preventDefault() previne as ações padrão do brownser.
 
     var form = document.querySelector("#form-adiciona");
-     var nome = form.nome.value;
-     var peso = form.peso.value;
-     var altura = form.altura.value;
-     var gordura = form.gordura.value;
-    
-    var pacienteTr = document.createElement("tr")
 
-    var nomeTd = document.createElement("td");
-    var pesoTd = document.createElement("td");
-    var alturaTd = document.createElement("td");
-    var gorduraTd = document.createElement("td");
-    var imcTd = document.createElement("td");
+    var paciente = obtenhaPacientesDoFormulario(form);
 
-    nomeTd.textContent = nome;
-    pesoTd.textContent = peso;
-    alturaTd.textContent = altura;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calculaImc(peso,altura);
-
-    pacienteTr.appendChild(nomeTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+    var pacienteTr = montaTr(paciente);
 
     var tabela = document.querySelector("#tabela-pacientes");
     
@@ -44,4 +25,44 @@ botaoAdicionar.addEventListener("click", function(event) {
     Exemplo de função anonima.
 });*/
 
-var neomar = 12;
+function obtenhaPacientesDoFormulario(form) {
+    var paciente = {
+        nome: form.nome.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc: calculaImc(form.peso.value, form.altura.value)
+    }
+
+    return paciente;
+}
+
+function montaTr(paciente){
+    var pacienteTr = document.createElement("tr")
+    pacienteTr.classList.add("paciente");
+    /*linha acima coloca a classe paciente aos pacientes adcionados pelo formulario*/
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+    /*parte 2 cria o tr e o td para cada paciente*/
+
+    /*3*/
+    nomeTd.textContent = paciente.nome;
+    pesoTd.textContent = paciente.peso;
+    alturaTd.textContent = paciente.altura;
+    gorduraTd.textContent = paciente.gordura;
+    imcTd.textContent = paciente.imc;
+    /*Parte 3 coloca os valores no td*/
+
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    return pacienteTr;
+}
+/*video 7.33*/
