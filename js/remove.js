933dcf9f8dacfb92fb18577428608c30f03a114b
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-11-07 22:33:39 -0300

    07112022
    Acrescentado o remove no site.

---
+++ js/remove.js
@@ -0,0 +1,9 @@
+var pacientes = document.querySelectorAll(".paciente");
+var tabela = document.querySelector("table");
+
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode; // = paciente = remover
+    paiDoAlvo.remove()
+    //event.target.parentNode.remove();    linha abreviada ela substitui as tres linhas acima.
+})
