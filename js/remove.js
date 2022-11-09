f30da764dbbd22563792abc01d2c647ef6efd3e8
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-11-08 23:38:03 -0300

    08112022
    Ajustes no remove.

--- js/remove.js
+++ js/remove.js
@@ -1,9 +1,10 @@
-var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
-
 tabela.addEventListener("dblclick", function(event){
-    var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode; // = paciente = remover
-    paiDoAlvo.remove()
-    //event.target.parentNode.remove();    linha abreviada ela substitui as tres linhas acima.
-})
+    event.target.parentNode.classList.add("fadeOut");
+    setTimeout(function(){
+        var alvoEvento = event.target;
+        var paiDoAlvo = alvoEvento.parentNode; // = paciente = remover
+        paiDoAlvo.remove()
+        //event.target.parentNode.remove();    linha abreviada ela substitui as tres linhas acima.
+    },500);
+});
