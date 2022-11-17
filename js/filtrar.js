c059d134e5e0a93d25208bb12fdc12f1d60a75f3
neomarpfleger <114037813+neomarpfleger@users.noreply.github.com>
2022-11-16 23:04:12 -0300

    16112022
    Modificação no campo de busca do website.

--- js/filtrar.js
+++ js/filtrar.js
@@ -2,10 +2,22 @@ var campoFiltro = document.querySelector("#filtrarTabela");
 campoFiltro.addEventListener("input", function(){
     console.log(this.value);//"this" é referente ao dono do evento que no caso seria a var campoFiltro.
     var pacientes = document.querySelectorAll(".paciente")
-    for( var i = 1; i < pacientes.length; i++){
-        var paciente = pacientes[i]
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = tdNome.textContent;
-        console.log(nome)
+
+    if(this.value.length > 0){
+        for( var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i]
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            if(nome != this.value){
+                paciente.classList.add("invisivel")
+            }else{
+                paciente.classList.remove("invisivel")
+            }
+        }
+    }else{
+        for(var i = 0; 1 < pacientes.length;i++){
+            var paciente = pacientes[i];
+            paciente.classList.remove("invisivel");
+        }
     }
 });
